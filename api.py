1d11e20eff8ed3fe108d49c974d41a766adad2e3
吴圳彬 <1137057407@qq.com>
2019-02-20 18:14:58 +0800

    video api

--- api.py
+++ api.py
@@ -8,3 +8,12 @@ https://aweme-eagle.snssdk.com/aweme/v1/user/?user_id=101209612312
 
 # 作品列表
 https://aweme.snssdk.com/aweme/v1/aweme/post/?user_id=101209612312
+
+# 视频地址
+https://aweme-hl.snssdk.com/aweme
+
+
+https://aweme-hl.snssdk.com/aweme/v1/play/?video_id=v0200faf0000bh0ajtej5ugu85rqd0vg&line=1&ratio=540p&watermark=1&media_type=4&vr_type=0&test_cdn=None&improve_bitrate=0&logo_name=aweme_suffix&device_platform=android&device_type=SM-G955N&version_code=500&device_id=62694298322&channel=wandoujia_aweme&aid=1128&pass-region=1&pass-route=1
+
+# 这才是真正的视频地址
+http://v6-dy.ixigua.com/a58b24bda5d961dd9cb719355d017141/5c6d35d1/video/m/220bf5c1d921dec43b7adb38fc54a8aacbf116144c5400006c48080ff886/?rc=ajN0OHh4bXdkazMzaWkzM0ApQHR7aXhicHpAb0hINDc1OTczNDY0OzM1PDNAKXUpQGczdylAZmxkamV6aGhkZjs0QHNhbzVkMmJnLV8tLV4tL3NzLW8jZmNwYmJebyM%2BMC0vLi0uLS0tLi4vLS4vaTpiLW8jOmAtbyNtbCtiK2p0OiMvLl4%3D
